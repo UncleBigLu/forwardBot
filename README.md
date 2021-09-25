462c23939b3d1ec5460ee3f443e16bfc28d4641d
unclebiglu <1807814821@qq.com>
2021-09-25 19:57:40 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Forward bot base on OPQBot and Botoy
+Forward specific message from one group to another.
+Remember to set the required environment varibles before execute this program(qq id, group id, etc).
---
+++ forwardBot.py
@@ -0,0 +1,44 @@
+from botoy import Action, Botoy, GroupMsg
+from botoy import decorators as deco
+import os
+import re
+
+
+bot = Botoy()
+fromGroup = os.environ['fromGroup']
+toGroup = os.environ['toGroup']
+
+botQQ = os.environ['botQQ']
+action = Action(botQQ)
+
+pattern = r'.*全体成员.*大学习.*'
+myRe = re.compile(pattern)
+# Message with @全体成员 would cause ctx.Content in the format of a dict.
+# Add a regex to judge if it's a dict and extract message from this dict string.
+dictPattern = r'^\{"Content":"(.*)","UserExt".*\}$'
+dictRe = re.compile(dictPattern)
+
+@bot.group_context_use
+def _(ctx):
+    # Block msg except from masterQQ
+    if str(ctx.FromGroupId) != fromGroup:
+        return
+    return ctx
+
+@bot.on_group_msg
+@deco.ignore_botself
+def get_msg(ctx):
+
+    action = Action(ctx.CurrentQQ)
+    
+    content = ctx.Content
+    if(myRe.match(content)):
+        m = dictRe.match(content)
+        if(m != None):
+            action.sendGroupText(int(toGroup), m.group(1))
+        else:
+            action.sendGroupText(int(toGroup), content)
+
+
+if __name__ == '__main__':
+    bot.run()
